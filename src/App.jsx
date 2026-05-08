a16e66227c39b00f577b26d2c42bdc4c0a35d9c0
Italicab <tony.gensale@italicab.it>
2026-05-08 14:24:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ export default function App() {
   <h1>{t.title}</h1>
   <p>{t.subtitle}</p>
   <a className="primary" href="mailto:info@italicab.it">{t.request}</a>
-  <a className="primary" href="/datasheets/ITALICAB-catalogue.pdf" target="_blank">Apri Catalogo PDF</a>
+  <a className="primary" href="/datasheets/ITALICAB-catalogue.pdf" target="_blank">{t.pdfCatalogue}</a>
 </div>
         <div className="contact"><b>ITALICAB</b><span>info@italicab.it</span><span>Tel. +39 030 6365625</span><span>Fax +39 030 6364819</span><span>Corso Cavour, 31 - 25121 Brescia</span></div>
       </section>
